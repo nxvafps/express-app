2ac9925651f23a3b9b7ba7e8fdb06ed0223aeb73
nxvafps <oliverquirk11@gmail.com>
2024-11-09 19:30:44 +0000

    Removing cors

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,13 +3,7 @@ import express from "express";
 import helmet from "helmet";
 
 const setupMiddlewares = (app) => {
-  app.use(
-    cors({
-      origin: "https://novafps.com",
-      methods: "GET",
-      allowedHeaders: "Content-Type, Authorization",
-    })
-  );
+  app.use(cors());
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
