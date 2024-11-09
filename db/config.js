c193310db323570bb1a82a5dd95191a3c80f5403
nxvafps <oliverquirk11@gmail.com>
2024-11-09 18:37:37 +0000

    Update database config

--- db/config.js
+++ db/config.js
@@ -6,7 +6,11 @@ const { Pool } = pkg;
 config();
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 pool.connect((err, client, release) => {
